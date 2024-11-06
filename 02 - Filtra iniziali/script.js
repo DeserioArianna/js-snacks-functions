569ac31d0b2b235906389af9b7bc9713310ecc12
DeserioArianna <arianna.deserio@gmail.com>
2024-11-06 18:09:57 +0100

    feat:fine secondo esercizio

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,11 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filtraLetteraConParola(array, lettera) {
+    return array.filter(parola => parola.charAt(0).toLowerCase() === lettera.toLowerCase());
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(filtraLetteraConParola(names, "a"))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
